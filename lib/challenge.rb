f72aa67415de69de9e9717bc7841f839ef6f8ec9
shkherad <shkherad@bu.edu>
2016-07-22 09:41:27 -0400

    challenge - almost done
    doesnt look for operations in middle of string

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,3 +1,23 @@
 def calc(expr)
   # Your code here
+  chars = expr.split(" ")
+
+  if chars[1] == nil
+    p 0
+
+  else
+    sum = chars[1].to_i
+    for i in 2..chars.length
+      sum = sum.send chars[0], chars[i].to_i
+      p sum
+    end
+
+    p "The total is #{sum}"
+
+  end
+
+
+
 end
+
+calc("* 22 2 3 11")
